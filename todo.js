04e47386fc6762e44da1c3f8fde95e03d263ac1f
kangyoungju <eodwn2@naver.com>
2020-01-07 23:40:07 +0900

    item 30개까지 등록

--- todo.js
+++ todo.js
@@ -3,12 +3,6 @@ var input = document.getElementById("input");
 var deleteAll = document.getElementById("deleteAll");
 var checkAll = document.getElementById("checkAll");
 
-// var deleteButton = document.createElement("button");
-//
-// deleteButton.addEventListener('click', function () {
-//     list.removeChild(li);
-// });
-
 deleteAll.addEventListener('click', function () {
     while(list.children.length > 0){
         list.removeChild(list.firstChild)
@@ -16,10 +10,19 @@ deleteAll.addEventListener('click', function () {
 });
 
 document.getElementById("addButton").addEventListener('click', function (){
+    addTodoItem();
+});
+
+var addTodoItem = function(){
+    var value = input.value;
+    if(value.trim() === ""){
+        alert("할일을 입력해 주세요.");
+
+        return;
+    }
+
     var li = document.createElement("li");
     var span = document.createElement("span");
-    var value = input.value;
-
     var deleteButton = document.createElement("button");
     deleteButton.type = "button";
     deleteButton.innerHTML="삭제";
@@ -28,11 +31,21 @@ document.getElementById("addButton").addEventListener('click', function (){
     inputCheck.type = "checkbox";
     inputCheck.className = "inputCheck";
 
+    var addButton = document.getElementById("addButton");
+
+    if(list.children.length > 29){
+        alert("할일은 최대 30개까지 추가 가능합니다.");
+
+        input.disabled = "disabled";
+        addButton.disabled = "disabled";
+
+        return;
+    }
+
     list.appendChild(li);
     li.append(inputCheck, span, deleteButton);
-    span.append(value);
-});
-
+    span.append(value);  
+}
 
 var getParentNode = function(target) {
     return target.tagName === "LI" ? target : target.parentNode;
@@ -42,23 +55,24 @@ list.addEventListener('click', function (event){
     var selectList = event.currentTarget;
     var selectLi = getParentNode(event.target);
     var selectInput = selectLi.getElementsByTagName("input");
+    var selectSpan = selectLi.getElementsByTagName("span");
 
     // TODO 삭제 버튼 클릭시 체크박스 체크가 되지 않아야한다.
     if(event.target.tagName !== "INPUT"){
         if(selectInput[0].checked) {
             selectInput[0].checked = "";
+            selectSpan[0].className = "";
         } else{
             selectInput[0].checked = "checked";
+            selectSpan[0].className = "completed";
         }
     }
 
     if(event.target.tagName === "BUTTON") {
         selectList.removeChild(selectLi);
     }
-
 });
 
-// 전체 체크박스
 checkAll.addEventListener('click', function () {
     var input = list.querySelectorAll("input");
 
@@ -69,4 +83,10 @@ checkAll.addEventListener('click', function () {
             item.checked = "";
         }
     });  
+});
+
+input.addEventListener('keyup', function(e){
+    if(e.keyCode===13){
+        addTodoItem();
+    }
 });
